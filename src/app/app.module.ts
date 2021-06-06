cc6c6f8afdf9159abdad7cfcff0ebc88bc0497ff
Jason L. Walker <jasonlanewalker@gmail.com>
2021-06-05 20:53:03 -0500

    added routing to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { TodaysTasksComponent } from './todays-tasks/todays-tasks.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'tasks', component: TodaysTasksComponent},
+  {path: 'lists', component: TodaysTasksComponent},
+  {path: '**', redirectTo: 'tasks'}
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +24,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     FontAwesomeModule,
     FormsModule,
     ModalModule.forRoot(),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
